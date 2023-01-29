30b4080d3f1fbeb25da2dbffbdad44f2b682d52c
sgalindo08 <sgalindo08@icloud.com>
2023-01-28 21:35:53 -0500

    Added an extra location

--- javascript/app.js
+++ javascript/app.js
@@ -1,51 +1,55 @@
-function updateTime() {
-  //rome
-  let romeElement = document.querySelector("#rome");
-  if (romeElement) {
-    let romeDateElement = romeElement.querySelector(".date");
-    let romeTimeElement = romeElement.querySelector(".time");
-    let romeTime = moment().tz("Europe/Rome");
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.40/moment-timezone-with-data-1970-2030.min.js"></script>
+    <title>World Clock - Stacy Galindo</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>World Clock</h1>
+      <select id="city">
+        <option value="">Choose a city</option>
+        <option value="current">My current location</option>
+        <option value="Europe/Madrid">Madrid</option>
+        <option value="America/New_York">New York</option>
+        <option value="Asia/Tokyo">Tokyo</option>
+      </select>
 
-    romeDateElement.innerHTML = romeTime.format("MMMM Do YYYY");
-    romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>]A[</small>]");
-  }
-  //dubai
-  let dubaiElement = document.querySelector("#dubai");
-  if (dubaiElement) {
-    let dubaiDateElement = dubaiElement.querySelector(".date");
-    let dubaiTimeElement = dubaiElement.querySelector(".time");
-    let dubaiTime = moment().tz("Asia/Dubai");
-
-    dubaiDateElement.innerHTML = dubaiTime.format("MMMM Do YYYY");
-    dubaiTimeElement.innerHTML = dubaiTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
-  }
-}
-function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-  <div class="city">
+      <div id="cities">
+        <div class="city" id="sydney">
           <div>
-            <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            <h2>Sydney🇦🇺</h2>
+            <div class="date"></div>
           </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )} <small>${cityTime.format("A")}</small></div>
+          <div class="time"></div>
         </div>
-        <a href="/">All cities</a>
-        `;
-}
-updateTime();
-setInterval(updateTime, 1000);
-
-let citiesSelectElement = document.querySelector("#city");
-
-citiesSelectElement.addEventListener("change", updateCity);
+        <div class="city" id="dubai">
+          <div>
+            <h2>Dubai🇦🇪</h2>
+            <div class="date"></div>
+          </div>
+          <div class="time"></div>
+        </div>
+        <div class="city" id="tokyo">
+          <div>
+            <h2>Tokyo🇯🇵</h2>
+            <div class="date"></div>
+          </div>
+          <div class="time"></div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      This project was coded by <strong>Stacy Galindo</strong> and is
+      <a href="https://github.com/sgalindo08?tab=repositories" target="_blank">
+        open source on GitHub.</a
+      >
+    </footer>
+    <script src="javascript/app.js"></script>
+  </body>
+</html>
